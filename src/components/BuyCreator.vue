a8e27ce7b19ef1b8d9239f31edb4530dee2ea891
martapla <martaplanella@hotmail.com>
2023-03-10 12:17:16 +0100

    first commit

---
+++ src/components/BuyCreator.vue
@@ -0,0 +1,83 @@
+<script  setup>
+import { reactive, defineEmits } from "vue"; //emit step 2
+
+const emit = defineEmits(["create-tobuy"]); // emit step 1
+
+const tobuyState = reactive({
+    tobuy: "",
+    invalid: null, //null = not value assigned
+    errMsg: "",
+
+});
+
+const createBuy = () => {           //emit step 3
+    tobuyState.invalid = null;
+    if (tobuyState.tobuy !== "") {  // if is not equal to an empty string
+        emit("create-tobuy", tobuyState.tobuy);
+        tobuyState.tobuy = ""; //reset to an empty string
+        return;
+    } 
+    tobuyState.invalid = true;            
+    tobuyState.errMsg = "...food missing... ☝ "
+    
+    
+};
+
+//https://www.youtube.com/watch?v=KTFH4P8unUQ min:51
+
+// Done with REF, when is only one element
+// const tobuy = ref("");
+
+// const createBuy = () => {  //emit step 3
+//     emit("create-tobuy", tobuy.value); //We put .value with Ref, not need .value with reactive
+//};
+
+</script>
+
+<template>
+    <section>
+    <div class="input-wrap">
+        <input type="text" v-model="tobuyState.tobuy"/>
+        <button @click="createBuy()">Add</button>
+    
+    </div>
+    <p v-show = "tobuyState.invalid" class="errMsg">{{ tobuyState.errMsg }}</p>
+    </section>
+</template>
+
+<style scoped>
+.input-wrap {
+    display: flex;
+    border:2px solid rgb(238, 134, 223);
+    border-radius: 3px;
+    transition:250ms ease;
+    width:80%;
+    height: 30px;
+    margin:0 auto;
+}
+
+input {
+    width:100%;
+    border:none;
+    padding: 8px 6px;
+}
+
+button {
+    border:none;
+    width:80px;
+    color:rgb(241, 55, 170);
+    background: rgb(146, 236, 176);
+}
+
+.errMsg {
+    margin-left: 150px;
+}
+</style>
+
+<!-- value:null -->
+
+<!-- null represents the absence of a value or the lack of a value.
+
+When a variable is assigned a value of null, it means that the variable is intentionally empty and doesn't point to any object or data.
+
+null can also be used to indicate an error condition or to signal the end of a data stream or list. It is often used in conditional statements and logical expressions to check if a variable contains a value or not. -->
---
+++ src/components/ShoppingHeader.vue
@@ -0,0 +1,56 @@
+<script setup></script>
+
+<template>
+    <header>
+        <nav class="container">
+            <div class="title">
+                <h1>Food Shopping <span> 🥕 🍅 🥑</span></h1>
+            </div>
+            <ul class="nav-routes">
+                <RouterLink to='/'>Home 🏡 </RouterLink>
+                <RouterLink to='/about'>Recipes 🥣 </RouterLink>
+            </ul>
+            
+        </nav>
+    </header>
+</template>
+
+
+<style scoped>
+/* *{
+    border:1px solid red;
+} */
+.container {
+    /* background-color: rgb(245, 165, 205); */
+    background-color: rgb(244, 213, 99);
+    display:flex;
+    width: 100%;
+    margin:0 auto;
+
+}
+.title {
+  text-align:left;
+  margin-left: 20px;
+  width: 50%;
+  color:rgb(241, 55, 170);
+  text-shadow: 2px 2px greenyellow;
+}
+span {
+    text-shadow: none;
+}
+.nav-routes {
+    display: flex;
+    align-content: center;
+    align-items: center;
+    justify-content:space-around;
+    width:35%;
+    margin:10 auto;
+}
+.nav-routes a {
+    text-decoration: none;
+    color:white;
+    font-weight: 700;
+    font-size: 17px;
+    
+}
+</style>
